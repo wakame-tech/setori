490b39ea96532b54831907b3eb68d609711e7b48
wakame-tech <kamata1919@gmail.com>
2020-01-15 23:37:45 +0900

    Initial Commit

---
+++ setori/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  setori
+//
+//  Created by 鎌田大己 on 2020/01/15.
+//  Copyright © 2020 wakame-tech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
