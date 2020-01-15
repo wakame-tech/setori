2d61695bd4e06b792e81bde5a69005163d33e7e6
wakame-tech <kamata1919@gmail.com>
2020-01-16 01:26:57 +0900

    [Add] minimal firestore sample

---
+++ setori/Redux/App.swift
@@ -0,0 +1,9 @@
+//
+//  App.swift
+//  setori
+//
+//  Created by 鎌田大己 on 2020/01/16.
+//  Copyright © 2020 wakame-tech. All rights reserved.
+//
+
+import Foundation
---
+++ setori/Redux/Store.swift
@@ -0,0 +1,9 @@
+//
+//  Store.swift
+//  setori
+//
+//  Created by 鎌田大己 on 2020/01/16.
+//  Copyright © 2020 wakame-tech. All rights reserved.
+//
+
+import Foundation
